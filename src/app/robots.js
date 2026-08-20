9559e42d6b9b31a5ce70dbd19d26a39e3dc6ec89
So <sshqn651@gmail.com>
2026-08-20 12:31:48 +0200

    Update sitemap URL in robots.js

--- src/app/robots.js
+++ src/app/robots.js
@@ -4,6 +4,6 @@ export default function robots() {
             userAgent: "*",
             allow: "/",
         },
-        sitemap: "https://festiny.netlify.app/sitemap.xml",
+        sitemap: "https://festiny.netlify.app/sitemap.js",
     };
 }
